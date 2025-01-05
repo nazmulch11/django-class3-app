24aff54762ef4a77b23cee064492ddd725449c94
nazmulch11 <nazmul.ch11@gmail.com>
2025-01-05 12:58:09 +0600

    class 3 code push
    Here You will find full code of class 3

---
+++ tasks/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
+from .models import Task
+from .forms import TaskForm
+
+from django.views.generic import ListView, DetailView, \
+    CreateView, UpdateView, DeleteView
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetailView(DetailView):
+    model = Task
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('tasks:task_list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy('tasks:task_list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks:task_list')
